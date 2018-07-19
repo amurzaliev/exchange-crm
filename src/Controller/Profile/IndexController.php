de57049b784aaae61001b82fe2d52c582ab020e1
votsera <votseraamid@gmail.com>
2018-07-19 20:48:29 +0600

    #6. Установка и настройка Admin LTE Была загружена библиотека Admin LTE. Добавлен IndexController.php  в /src/Controller/Profile/. В IndexController.php добален indexAction() для рендеринга индексной страницы. Добавлены файлы js, css, img, fonts библиотеки AdminLTE.

---
+++ src/Controller/Profile/IndexController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller\Profile;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class IndexController extends Controller
+{
+    /**
+     * @Route("/profile", name="profile_index")
+     */
+    public function indexAction()
+    {
+        return $this->render('profile/index/index.html.twig', []);
+    }
+}
