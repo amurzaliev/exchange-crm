d759bd4de06737cb9a29e961f1e2254fee12210b
maks <limarevqw@gmail.com>
2018-07-28 16:44:41 +0600

    #10 Добавлен StaffController и базовые методы CRUD, и ещё добавлены соответствующие twig файлы

---
+++ src/Controller/Profile/StaffController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Controller\Profile;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * Class StaffController
+ * @package App\Controller\Profile
+ *
+ * @Route("profile/staff")
+ */
+class StaffController extends Controller
+{
+
+    /**
+     * @Route("/", name="profile_staff_index")
+     * @Method({"GET"})
+     * @return Response
+     */
+    public function indexAction()
+    {
+        return $this->render('profile/staff/index.html.twig');
+    }
+
+    /**
+     * @Route("/create", name="profile_staff_create")
+     * @return Response
+     * @Method({"GET", "POST"})
+     */
+    public function createAction()
+    {
+        return $this->render('profile/staff/create.html.twig');
+    }
+
+    /**
+     * @Route("/{id}/edit", name="profile_staff_edit", requirements={"id"="\d+"})
+     * @Method({"GET", "PUT"})
+     * @param int $id
+     * @return Response
+     */
+    public function editAction(int $id)
+    {
+        return $this->render('profile/staff/edit.html.twig');
+    }
+
+    /**
+     * @Route("/{id}/show", name="profile_staff_show", requirements={"id"="\d+"})
+     * @param int $id
+     * @Method({"GET"})
+     * @return Response
+     */
+    public function showAction(int $id)
+    {
+        return $this->render('profile/staff/show.html.twig');
+    }
+}
